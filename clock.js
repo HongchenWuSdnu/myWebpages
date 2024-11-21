4f1075fd527aa0ba6b0086f17bfdd9005b6b0dc0
wuhockey <154344252+wuhockey@users.noreply.github.com>
2024-11-21 22:27:47 +0800

    Update clock.js

--- clock.js
+++ clock.js
@@ -35,7 +35,7 @@ const data = {
            `${this.hours < 10 ? '0' + this.hours : this.hours}:${this.minutes < 10 ? '0' + this.minutes : this.minutes}:${this.seconds < 10 ? '0' + this.seconds : this.seconds}.${this.milliseconds < 100 ? '0' + (this.milliseconds < 10 ? '0' + this.milliseconds : this.milliseconds) : this.milliseconds} ` +
            `时区: ${this.timeZoneOffset}`;
 
-    // 如果分钟数为50，输出“还差十秒一分钟”
+    // 如果秒数为50，输出“还差十秒一分钟”
     if (this.seconds === 50) {
       timeString += "\n还差十秒一分钟！";
     }
